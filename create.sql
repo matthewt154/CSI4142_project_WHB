85bad0faa1d9992efb383fb1a648020c0e80d461
Tate S <tate.sharp@gmail.com>
2022-03-18 16:43:45 -0400

    Add fact table / Months / Country table generation script. Include the related .csv's.

---
+++ create.sql
@@ -0,0 +1,101 @@
+------------------------- Create 'Months' Table
+DROP TABLE IF EXISTS Months CASCADE;
+
+CREATE TABLE IF NOT EXISTS Months
+(
+    Name text NOT NULL,
+    Quarter bigint NOT NULL,
+    Year integer NOT NULL
+);
+
+COPY Months(name, Quarter, Year)
+FROM 'D:\University\5thYear\4142_bulkdata\Month.csv' -- Edit local path
+DELIMITER ',' CSV HEADER;
+
+ALTER TABLE Months ADD COLUMN monthkey serial PRIMARY KEY;
+
+------------------------- Create 'Country' Table
+DROP TABLE IF EXISTS country CASCADE;
+
+CREATE TABLE IF NOT EXISTS country
+(
+    countrycode text NOT NULL UNIQUE,
+    countryname text ,
+    capital text ,
+    currency text ,
+    region text ,
+    continent text ,
+    incomegroup text,
+    population bigint,
+    birthrate bigint,
+    deathrate bigint
+);
+
+COPY country(countrycode, countryname, currency, region, incomegroup)
+FROM 'D:\University\5thYear\4142_bulkdata\HNP_StatsCountry.csv' -- Edit local path
+DELIMITER ',' CSV HEADER;
+
+ALTER TABLE country ADD COLUMN countrykey serial PRIMARY KEY;
+
+-- Delete entries of countries we aren't including
+DELETE from country WHERE country.countryname NOT IN ('Canada', 'United States', 
+						  'Brazil', 'Mexico', 'Niger',
+						  'Syrian Arab Republic', 'South Africa', 'Thailand');
+						  
+
+
+------------------------- Create Fact Table
+DROP TABLE IF EXISTS fact;
+
+SELECT m.monthkey, c.countrycode
+INTO fact
+from months m, Country c;
+
+ALTER TABLE fact ADD COLUMN factkey serial PRIMARY KEY;
+
+ALTER TABLE fact
+	ADD CONSTRAINT fk_countrycode FOREIGN KEY (countrycode) REFERENCES country(countrycode);
+	
+ALTER TABLE fact
+	ADD CONSTRAINT fk_monthkey FOREIGN KEY (monthkey) REFERENCES months(monthkey);
+
+------------------------- Load Development index data & create table
+drop table if exists index_dev;
+
+create table index_dev (
+	countryName text NOT NULL,
+	countrycode text NOT NULL,
+	Year integer NOT NULL,
+	DevelopmentIndex decimal
+);
+
+COPY index_dev(countryName, countrycode, Year, DevelopmentIndex) 
+FROM 'D:\University\5thYear\4142_bulkdata\development_index_v2.csv' -- Edit local path
+DELIMITER ',' CSV HEADER;
+
+-------------------------  Create temp table to migrate development index data
+drop table if exists tmp;
+
+create table if not exists tmp (
+	factkey serial PRIMARY KEY,
+	developmentindex decimal);
+	
+------------------------- Populate temp table	
+insert into tmp
+	SELECT factkey, index_dev.developmentindex from fact
+	INNER JOIN index_dev 
+		ON index_dev.countrycode = fact.countrycode
+	INNER JOIN Months m ON m.monthkey = fact.monthkey
+		WHERE m.Year = index_dev.Year;
+-----------------------------------------------
+alter table fact drop if exists developmentindex;
+alter table fact add developmentindex numeric;
+
+------------------------- Populate fact table with the 'DevelopmentIndex' attribute
+update fact as f
+set developmentindex = t.developmentindex
+from tmp as t
+where t.factkey = f.factkey;
+
+
+select * from fact order by monthkey;
